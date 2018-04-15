789d8ed6b000c53014342d744db56bcd9e5ad7b5
aaa7789 <rajireddy.kr@gmail.com>
2018-04-15 12:09:12 -0400

    added by Raji into bubby reposioty

--- src/main/java/com/mms/first/Spring_Test.java
+++ src/main/java/com/mms/first/Spring_Test.java
@@ -3,6 +3,7 @@ package com.mms.first;
 public class Spring_Test {
     public static void main(String[] args) {
         System.out.println("this added by Raji");
+        System.out.println("this is second commit");
     }
 
 }
